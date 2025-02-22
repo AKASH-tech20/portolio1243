48c6402a47c10758b1a6218167ae9cf6baeca28c
AKASH-tech20 <marvelu767@gmail.com>
2025-02-22 09:31:59 -0800

    udated html

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portolio1243/",
+  base: "/threejs-portfolio/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "src/main.jsx",
+    },
+  },
 });
