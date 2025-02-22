a9854c910a19a1685dc30f8d1f0f80e9cf43d9db
AKASH-tech20 <marvelu767@gmail.com>
2025-02-22 08:58:33 -0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: "src/main.jsx",
-    },
-    outDir: "dist",
-  },
+  base: "/portolio1243/",
 });
